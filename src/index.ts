5cd7779fb41c8fd746a1762edb1efbf0c99081f3
RAFA <rafaelmoncayop37@gmail.com>
2024-09-26 14:40:11 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@ const inputText = document.getElementById('inputText') as HTMLInputElement;
 const createBtn = document.getElementById('createBtn') as HTMLButtonElement;
 const cardContainer = document.getElementById('cardContainer') as HTMLElement;
 
+function fechtimage() { 
+  new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        resolve('https://picsum.photos/200/300'); 
+      } else {
+        reject('Failed to fetch image.'); 
+      }
+    }, 2000);
+  }
+  )
+  .then((resolvedImage) => {
+    const image = document.createElement('img');
+    image.src = resolvedImage; 
+    cardContainer.appendChild(image);
+  })
+  .catch((error) => {
+    alert(error);
+    console.error(error);
+  });
+
+}
+
+createBtn.addEventListener('click', fechtimage);
+
+
 function createCard(): void {
   const cardText = inputText.value; 
 
@@ -9,18 +35,33 @@ function createCard(): void {
     alert('Please enter some text.');
     return;
   }
+  new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        resolve(cardText); 
+      } else {
+        reject('Failed to create card.'); 
+      }
+    }, 2000);
+  })
+  .then((resolvedText) => {
+   
+    const card = document.createElement('ion-card');
+    const cardHeader = document.createElement('ion-card-header');
+    const cardTitle = document.createElement('ion-card-title');
+    cardTitle.textContent = resolvedText; 
 
-  const card = document.createElement('ion-card');
-  const cardHeader = document.createElement('ion-card-header');
-  const cardTitle = document.createElement('ion-card-title');
-  cardTitle.textContent = cardText;
-  
-  cardHeader.appendChild(cardTitle);
-  card.appendChild(cardHeader);
-  
-  cardContainer.appendChild(card);
+    cardHeader.appendChild(cardTitle);
+    card.appendChild(cardHeader);
+    cardContainer.appendChild(card);
 
-  inputText.value = '';
+    inputText.value = ''; 
+  })
+  .catch((error) => {
+
+    alert(error);
+    console.error(error);
+  });
 }
 
 createBtn.addEventListener('click', createCard);
